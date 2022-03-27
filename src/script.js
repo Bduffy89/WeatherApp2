6ebb64b884d2e03aa084dd6e8ca1c9f2848f153f
Bduffy89 <97573421+Bduffy89@users.noreply.github.com>
2022-03-27 11:29:22 -0400

    Added time/day JS

---
+++ src/script.js
@@ -0,0 +1,23 @@
+//Time
+
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let h4 = document.querySelector("h4");
+let day = days[now.getDay()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minute = now.getMinutes();
+if (minute < 10) {
+  minute = `0${minute}`;
+}
+h4.innerHTML = `${day}, ${hour}:${minute}`;
