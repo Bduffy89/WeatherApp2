2fb621b450d2562ec4567e028ae93ae8bc3af832
Bduffy89 <97573421+Bduffy89@users.noreply.github.com>
2022-03-27 12:10:14 -0400

    Fixed issue with weather icon and description not populating, set home weather location

--- src/script.js
+++ src/script.js
@@ -81,3 +81,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelciusTemp);
+
+search("Mombasa");
