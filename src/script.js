8634aa4e004db6f28331befac801f6630ae792ce
Bduffy89 <97573421+Bduffy89@users.noreply.github.com>
2022-03-27 12:05:34 -0400

    Fixed issue with search result not populating

--- src/script.js
+++ src/script.js
@@ -21,3 +21,63 @@ if (minute < 10) {
   minute = `0${minute}`;
 }
 h4.innerHTML = `${day}, ${hour}:${minute}`;
+
+//Search Location Temp Results
+
+function displayTemp(response) {
+  console.log(response.data);
+
+  let temperatureElement = document.querySelector("#temp");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#weatherIcon");
+  let weatherElement = document.querySelector("#weather-description");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  weatherElement.innerHTML = response.data.weather[0].description;
+}
+
+function search(city) {
+  let apiKey = "14b403338547ae85381bcc96119486e3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemp);
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-search-input");
+  search(searchInput.value);
+  let city = document.querySelector("#search-result");
+  city.innerHTML = searchInput.value;
+}
+
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+  let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
+
+function displayCelciusTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-bar");
+form.addEventListener("submit", searchCity);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelciusTemp);
