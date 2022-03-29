5776dc14d1f547153bad9e2ab2cb4f4e92f0722e
Bduffy89 <97573421+Bduffy89@users.noreply.github.com>
2022-03-28 22:41:09 -0400

    Functions for calling coordinate data for forecast

--- src/script.js
+++ src/script.js
@@ -24,6 +24,13 @@ h4.innerHTML = `${day}, ${hour}:${minute}`;
 
 //Search Location Temp Results
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "14b403338547ae85381bcc96119486e3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemp(response) {
   console.log(response.data);
 
@@ -43,6 +50,8 @@ function displayTemp(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   weatherElement.innerHTML = response.data.weather[0].description;
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -82,7 +91,8 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelciusTemp);
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -123,5 +133,4 @@ function displayForecast() {
   console.log(forecastHTML);
 }
 
-displayForecast();
 search("Mombasa");
